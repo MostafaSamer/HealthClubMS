8d9a34cbb9a8474aa62d7606733e3a1bf01618fb
moatasem.720 <moatasem.720@gmail.com>
2019-01-22 01:27:59 +0200

    third Update

--- src/GUI/Admin.java
+++ src/GUI/Admin.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.application.*;
@@ -16,22 +18,47 @@ import java.io.IOException;
 public class Admin extends Application {
 
     public static Stage stage;
-/*
+
     @FXML
-    public Button Coaches_Button ;
-    public Button See_Coaches_Button ;
-    public Button Add_Coaches_Button ;
-    public Pane See_Coaches_Pane ;
+    public MenuButton Coaches_Button ;
+    public MenuItem See_Coaches_Button ;
+    public MenuItem Add_Coaches_Button ;
     public Pane Add_Coaches_Pane ;
+    public Pane See_Coaches_Pane ;
+    public Pane See_members_Pane ;
+    public Pane Add_members_Pane ;
+
 
     public void Add_Coaches_method() {
         See_Coaches_Pane.setVisible(false);
         Add_Coaches_Pane.setVisible(true);
+        See_members_Pane.setVisible(false);
+        Add_members_Pane.setVisible(false);
     }
 
     public void See_Coaches_method() {
 
-    }*/
+        See_Coaches_Pane.setVisible(true);
+        Add_Coaches_Pane.setVisible(false);
+        See_members_Pane.setVisible(false);
+        Add_members_Pane.setVisible(false);
+    }
+    public void See_member_method() {
+        See_Coaches_Pane.setVisible(false);
+        Add_Coaches_Pane.setVisible(false);
+        See_members_Pane.setVisible(true);
+        Add_members_Pane.setVisible(false);
+
+    }
+
+    public void Add_member_method(){
+
+        See_Coaches_Pane.setVisible(false);
+        Add_Coaches_Pane.setVisible(false);
+        See_members_Pane.setVisible(false);
+        Add_members_Pane.setVisible(true);
+
+    }
 
 
     public static void main(String[] args) {
